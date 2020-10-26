699ee904ffbc78d28b54216bd1606b004f8204fd
Deadpool198 <danielwalters12345@gmail.com>
2020-10-26 11:03:38 +0000

    added python code

---
+++ code.py
@@ -0,0 +1,22 @@
+def add(a, *b):
+    for num in b:
+        a += num
+    return a
+
+def subtract(a, *b):
+    for num in b:
+        a -= num
+    return a:
+        a
+
+def multiply(a, *b):
+    for num in b:
+        a *= b
+    return a
+
+def divide(a, *b):
+    for num in b:
+        a /= b
+    return a 
+
+
